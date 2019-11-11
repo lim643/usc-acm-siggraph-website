000ee9b1bda788181293135d7b6d1b8749b3f178
Matthew Lim <lim643@usc.edu>
2019-11-10 18:49:39 -0800

    Initial commit

---
+++ js/events.js
@@ -0,0 +1,9 @@
+var numGallery = 8; //For photo gallery
+
+var num = 1; //For events
+
+var title1='Paul Debevec: \"Lighting Hollywood\'s Photoreal Digital Actors\"';
+var date1='2.24.16';
+var time1='6 pm';
+var place1='TCC 227';
+var body1='Come hear Paul Debevec, Chief Visual Officer of the USC Institute of Creative Technologies, talk about his work as part of Engineers Week! We will have free CREAM ice cream sandwiches at the event! So come enjoy a night of free ice cream and computer graphics. What can be better than that?';
---
+++ js/news.js
@@ -0,0 +1,4 @@
+var numNews =0;
+/*var newsTitle1='Student Volunteer Apps Deadline';
+var newsDate1='2.10.15';
+var newsBody1='Are you interested in attending the most world-renowned conference on computer graphics, 3D animation, visual effects, and interactive techniques? Are you interested in attending it for free? If you apply to become a SIGGRAPH student volunteer, you could do exactly this. Plus, you could gain free access to the largest and most important conference on computer graphics in the world, gain free ACM SIGGRAPH membership for an entire year, network with reps from different companies, and participate in resume reviews and student-oriented lectures. The deadline to apply is today, February 10th.';*/
